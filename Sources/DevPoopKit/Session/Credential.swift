85d4e904dd119be05a5370f50c1b882f7c888868
Austin Emmons <austin.emmons@modolabs.com>
2018-05-25 00:11:47 -0400

    Breaks out Credential struct

---
+++ Sources/DevPoopKit/Session/Credential.swift
@@ -0,0 +1,10 @@
+public struct Credential {
+    public init(username: String, password: String) {
+        self.username = username
+        self.password = password
+    }
+
+    public var username : String
+    public var password : String
+}
+
