844c2e9b2513aa163b9300abcd33bf344c87889d
Austin Emmons <austin.emmons@modolabs.com>
2018-05-25 00:07:43 -0400

    Updates AppRequest interface to for all actions

--- Sources/DevPoopKit/Session/Request/AppRequest.swift
+++ Sources/DevPoopKit/Session/Request/AppRequest.swift
@@ -2,13 +2,17 @@ import Alamofire
 import AlamofireSynchronous
 
 public class AppRequest {
-    public static func request(_ session: Session) -> DataRequest {
 
+    open static let base = "https://developer.apple.com/services-account/"
+    open static let versionPath = "QH65B2"
+    open static let platform = "ios"
+
+    public static func all(_ session: Session) -> DataRequest {
         let pageNumber = 1
         let pageSize   = 500
 
         return session.request(
-            "https://developer.apple.com/services-account/QH65B2/account/ios/identifiers/listAppIds.action",
+            "\(base)\(versionPath)/account/\(platform)/identifiers/listAppIds.action",
             method: .post,
             parameters: [
                 "teamId"     : session.selectedTeamId!,
@@ -19,10 +23,43 @@ public class AppRequest {
         )
     }
 
-    public static func detail(_ session: Session, appIdentifier: String) -> DataRequest {
-        print(appIdentifier)
+    public static func show(_ session: Session, appIdentifier: String) -> DataRequest {
         return session.request(
-            "https://developer.apple.com/services-account/QH65B2/account/ios/identifiers/getAppIdDetail.action",
+            "\(base)\(versionPath)/account/\(platform)/identifiers/getAppIdDetail.action",
+            method: .post,
+            parameters: [
+                "teamId"     : session.selectedTeamId!,
+                "appIdId"    : appIdentifier
+            ]
+        )
+    }
+
+    public static func create(_ session: Session, appIdentifier: String, name: String) -> DataRequest {
+        return session.request(
+            "\(base)\(versionPath)/account/\(platform)/identifiers/getAppIdDetail.action",
+            method: .post,
+            parameters: [
+                "teamId"     : session.selectedTeamId!,
+                "appIdId"    : appIdentifier
+            ]
+        )
+    }
+
+    public static func update(_ session: Session, appIdentifier: String, name: String) -> DataRequest {
+        return session.request(
+            "\(base)\(versionPath)/account/\(platform)/identifiers/updateAppIdName.action",
+            method: .post,
+            parameters: [
+                "teamId"     : session.selectedTeamId!,
+                "appIdId"    : appIdentifier,
+                name         : name
+            ]
+        )
+    }
+
+    public static func delete(_ session: Session, appIdentifier: String, name: String) -> DataRequest {
+        return session.request(
+            "\(base)\(versionPath)/account/\(platform)/identifiers/deleteAppId.action",
             method: .post,
             parameters: [
                 "teamId"     : session.selectedTeamId!,
