75dc1c9ae53e5a0b2a61734128eeb602e07f4f21
Austin Emmons <austin.emmons@modolabs.com>
2018-06-04 21:02:43 -0700

    Adds InviteRequest with all, create, and delete methods

---
+++ Sources/DevPoopKit/Session/Request/InviteRequest.swift
@@ -0,0 +1,69 @@
+import Alamofire
+import Foundation
+
+public class InviteRequest {
+    public enum TeamMemberRole : String {
+        case member = "MEMBER"
+        case admin  = "ADMIN"
+        case agent  = "AGENT"
+    }
+
+    public struct InviteDescriptor {
+        let email : String
+        let role  : TeamMemberRole
+    }
+
+    static let base = "https://developer.apple.com/services-account/"
+
+    public static func all(_ session: Session) -> DataRequest {
+        return session.request(
+            "\(base)\(session.configuration.apiVersion)/account/getInvites",
+            method: .post,
+            parameters: [
+                "teamId"     : session.selectedTeamId!,
+            ],
+            encoding: JSONEncoding.default,
+            headers: [
+                "Content-Type" : "application/json",
+            ]
+        )
+    }
+
+    public static func create(_ session: Session, invites: [InviteDescriptor]) -> DataRequest {
+        let inviteData = invites.map { invite in
+            return [
+                "recipientEmail" : invite.email,
+                "recipientRole" : invite.role.rawValue
+            ]
+        }
+
+        return session.request(
+            "\(base)\(session.configuration.apiVersion)/account/sendInvites",
+            method: .post,
+            parameters: [
+                "teamId"  : session.selectedTeamId!,
+                "invites" : inviteData
+            ],
+            encoding: JSONEncoding.default,
+            headers: [
+                "Content-Type" : "application/json",
+            ]
+        )
+    }
+
+    public static func delete(_ session: Session, inviteIdentifiers: [String]) -> DataRequest {
+        return session.request(
+            "\(base)\(session.configuration.apiVersion)/account/sendInvites",
+            method: .post,
+            parameters: [
+                "teamId"  : session.selectedTeamId!,
+                "inviteIds" : inviteIdentifiers
+            ],
+            encoding: JSONEncoding.default,
+            headers: [
+                "Content-Type" : "application/json",
+            ]
+        )
+    }
+
+}
